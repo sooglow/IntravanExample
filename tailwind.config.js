51dde3d2e42c42b51748f20110c1409799d6fbe8
[Github <kwonjisu1218@gmail.com>
2023-11-01 10:36:12 +0900

    커밋 메세지

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./html/**/*.html"], /* html파일 외 파일들을 검사한다.*/
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/*다운 터미널 -d 는 developer용으로 설치하는것*/
